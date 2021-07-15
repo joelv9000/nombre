2360e070f01db108c7ff9c0b5e31ceee3ba0cc6d
joelv9000 <joelvelazquezvargasmm@gmail.com>
2021-07-15 17:09:36 -0500

    nombre

---
+++ nombre/nombre/nombre.java
@@ -0,0 +1,21 @@
+package nombre;
+
+
+
+import java.util.Scanner;  
+public class nombre {
+
+    public static void main(String[] args) {  
+
+           Scanner sc = new Scanner(System.in); 
+           String nombre;
+           
+           System.out.print("Introduzca su nombre: ");       
+           nombre = sc.nextLine();  
+           System.out.println("Hola " + nombre + "!!! mi nombre es: Joel");
+           
+     }
+}
+	
+
+
